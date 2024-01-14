e83300bf2c3316b4d5603a5375968098edca17d5
rimshaali12345 <156575773+rimshaali12345@users.noreply.github.com>
2024-01-14 15:13:16 +0500

    Add files via upload

---
+++ faiza .py
@@ -0,0 +1,31 @@
+('''add
+ subtract 
+mltiply
+divide
+''' )
+num1=int(input('enter your value num1'))
+num2=int(input('enter your value num2'))
+opr=input('enter the opr')
+if opr=="+":
+ print (num1+num2)
+elif opr=="-":
+ print (num1-num2)
+elif opert=="*":
+    print(num1*num2)
+elif  opr=="/":
+    print(num1/num2)
+else:
+    ("invalid operater")
+
+     
+
+
+     
+
+
+     
+
+                
+                    
+                      
+        
